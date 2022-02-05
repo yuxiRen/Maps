32b6240a7ed1a8eb4d0b09243297ce70bf7c3be0
yuxiRen <rita.ren.yuxi@gmail.com>
2022-02-04 22:14:13 -0800

    import google.maps

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ const user = new User();
 console.log(user);
 const company = new Company();
 console.log(company);
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: { lat: 0, lng: 0 },
+});
